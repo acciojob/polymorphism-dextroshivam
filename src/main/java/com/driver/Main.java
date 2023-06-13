bc840db5353911d7a30c14e5f96fc354d32d9725
shivam <shivamp9148@gmail.com>
2023-06-13 15:28:24 +0530

    c5

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,33 @@
 package com.driver;
 
 public class Main {
-    public static class Product{
-        public int product(int x, int y, int z) {
-            return x+y+z;
+
+    public static class  Product{// task 1
+        public static int product(int x, int y) {//task 3
+            return x*y;
         }
-        public double product(double x, double y) {
-            return x+y;
+
+        public static int product(int x, int y, int z) {//task 4
+            return x*y*z;
         }
-        public int product(int x, int y) {
-            return x+y;
+        public static double product(double x, double y) {//task 5
+            return x*y;
         }
     }
+
     public static void main(String[] args) {
-        Product p=new Product();
-        p.product(2,3);
-        p.product(2,3,4);
-        p.product(2.2,3.2);
+
+        Product p = new Product();//task 2
+
+        p.product(1,2);
+        //System.out.println(p.product(1,2));
+
+        p.product(1,2,3);
+        //System.out.println(p.product(1,2,3));
+
+        p.product(1.000,2.000);
+        //System.out.println(p.product(1.000,2.000));
+
     }
 
 }
